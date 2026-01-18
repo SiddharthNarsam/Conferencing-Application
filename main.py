f25686eb0e9f399ae8099384a7920cf54fba4864
SiddharthNarsam <sknarsam@gmail.com>
2026-01-18 23:31:31 +0530

    basic 1-1 conferencing working

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+clients = []
+
+@app.get("/")
+async def index():
+    with open("static/index.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
+    clients.append(ws)
+
+    try:
+        while True:
+            data = await ws.receive_text()
+            for client in clients:
+                if client != ws:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(ws)
